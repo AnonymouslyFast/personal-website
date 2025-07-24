ca84c3105a7ef6d10d08a614f7f93e18b23f4ed7
AnonymouslyFast <calebshearer1@gmail.com>
2025-07-24 00:10:39 -0400

    Homepage mobile support hopefully

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
     import BackGround from "$lib/BackGround.svelte";
     import Navbar from "$lib/Navbar.svelte";
-    import FloatingContainer from "$lib/FloatingContainer.svelte";
 </script>
 
 <BackGround />
@@ -10,47 +9,140 @@
     <Navbar />
 
     <div id="main">
-        <FloatingContainer>
-            <div id="image-container">
-                <img id="title-image" src="unformal-image.jpeg" alt="Unformal selfie of me (Caleb Shearer) visibly wearing glasses and a green shirt."/>
-            </div>
-            <div id="title-container">
-                <h1 id="title">Hello, I'm Caleb Shearer</h1>
-                <h3 id="description">I'm a high school senior planning to major in Computer Engineering or Computer Science. In the future I'm planning to become a game developer, and I'm working hard to get to that goal.</h3>
-                <div id="connections-container">
-                    <a href="https://github.com/AnonymouslyFast" class="connection-btn"><img src="github-mark.svg" alt="Github icon"></a>
-                    <a href="https://www.youtube.com/@AnonymouslyFast" class="connection-btn"><img src="youtube-icon.svg" alt="Youtube icon"></a>
-                    <a href="mailto:fastskating12@gmail.com" class="connection-btn"><img src="email-icon.svg" alt="Email icon"></a>
+            <div id="main-content">
+                <div id="image-container">
+                    <img
+                            src="unformal-image.jpeg"
+                            alt="Unformal selfie of me (Caleb Shearer) visibly wearing glasses and a green shirt."
+                            id="title-image"
+                    />
+                </div>
+
+                <div id="title-container">
+                    <h1 id="title">
+                        Hello, I'm Caleb Shearer
+                    </h1>
+                    <p id="description">
+                        I'm a high school senior planning to major in Computer Engineering or Computer Science.
+                        In the future I'm planning to become a game developer, and I'm working hard to get to that goal.
+                    </p>
+
+                    <div id="connections-container">
+                        <a href="https://github.com/AnonymouslyFast" class="connection-btn">
+                            <img src="github-mark.svg" alt="GitHub" />
+                        </a>
+                        <a href="https://www.youtube.com/@AnonymouslyFast" class="connection-btn">
+                            <img src="youtube-icon.svg" alt="YouTube" />
+                        </a>
+                        <a href="mailto:fastskating12@gmail.com" class="connection-btn">
+                            <img src="email-icon.svg" alt="Email" />
+                        </a>
+                    </div>
                 </div>
             </div>
-        </FloatingContainer>
     </div>
-
 </div>
 
 <style>
+    
+    @media (max-width: 800px) {
+
+        #main {
+            width: 85vw !important;
+        }
+
+        #image-container {
+            width: 85vw !important;
+            justify-content: center;
+            margin-top: 4vw;
+        }
+
+        #title-image {
+            width: 40vw !important;
+            height: 55vw !important;
+            box-shadow:
+                    rgba(147, 147, 147, 0.4) 0px 5px,
+                    rgba(147, 147, 147, 0.3) 0px 10px,
+                    rgba(147, 147, 147, 0.2) 0px 15px,
+                    rgba(147, 147, 147, 0.1) 0px 20px,
+                    rgba(147, 147, 147, 0.05) 0px 25px !important;
+        }
+
+        #title-container {
+            width: 100% !important;
+            justify-self: center !important;
+            margin-left: 5vw !important;
+            margin-top: 2vw !important;
+        }
+
+        #connections-container {
+            margin-left: 0 !important;
+            margin-top: 2vw !important;
+            width: 60vw !important;
+            justify-self: center !important;
+        }
+
+        .connection-btn{
+            width: 40vw !important;
+            height: 5vw !important;
+            margin-right: 20% !important;
+        }
+
+        .connection-btn img {
+            width: 4vw !important;
+        }
+
+        #title {
+            justify-self: center !important;
+            font-size: 1rem !important;
+            width: 70vw !important;
+        }
+
+        #description {
+            width: 60vw !important;
+            justify-self: center !important;
+            font-size: 0.9rem !important;
+        }
+    }
+    
+    #container {
+        width: 100%;
+        height: 100%;
+    }
+
     #main {
         display: flex;
         flex-direction: column;
         width: 60vw;
-        height: 60vh;
+        padding: 3vw;
         justify-self: center;
-        margin-top: 5vh;
+        margin-top: 3rem;
+        background: #323232;
+        border-radius: 5vw;
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    }
+
+    #main-content {
+        width: 100%;
+        height: 100%;
+        flex: 1;
+        gap: 3rem;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        flex-wrap: wrap;
+        position: relative;
     }
 
     #image-container {
-        min-width: 0;
-        min-height: 0;
+        display: flex;
         flex: 1;
         position: relative;
-        top: 7vh;
-        left: 3vw;
-        width: 50vw;
-        padding-left: 2vw;
     }
 
     #title-image {
-        width: 30%;
+        width: 15vw;
+        height: 20vw;
         border-radius: 2vw;
         box-shadow:
                 rgba(147, 147, 147, 0.4) -5px 5px,
@@ -61,36 +153,35 @@
     }
 
     #title-container {
-        min-width: 0;
-        min-height: 0;
-        flex: 2;
-        width: 50%;
-        position: relative;
-        top: -27vh;
-        left: 25vw;
+        max-height: 100%;
+        flex: 1;
+        max-width: 50%;
+        margin-top: 10%;
+        margin-right: 10%;
         text-align: center;
     }
 
     #title {
-        font-size: clamp(1rem, 3vw, 1.5rem);
+        font-size: clamp(0.5rem, 1.5rem, 2rem);
         font-weight: bold
     }
 
     #description {
-        font-size: clamp(0.5rem, 1.5vw, 1rem);
+        font-size: clamp(0.5rem, 1rem, 1rem);
         font-weight: lighter;
     }
 
     #connections-container {
         display: inline-flex;
         justify-content: space-evenly;
-        margin-top: 3vh;
+        margin-top: 1rem;
         width: 100%;
     }
 
     .connection-btn {
-        width: 4vw;
-        height: 5vh;
+        margin-left: 1vw;
+        width: 3vw;
+        height: 2vw;
         border-radius: 5vw;
         box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
         justify-content: center;
@@ -103,11 +194,7 @@
     }
 
     .connection-btn img {
-        width: 30%;
-        margin-top: 1vh;
+        margin-top: 0.4vw;
+        width: 40%;
     }
-
 </style>
-
-
-
