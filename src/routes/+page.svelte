317c181adf0a3be0fe7172f408bed3b9001ca53f
AnonymouslyFast <calebshearer1@gmail.com>
2025-07-24 06:29:31 -0400

    Hopefully fixes mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -104,12 +104,14 @@
         }
 
         #title {
+            position: relative !important;
             justify-self: center !important;
             width: 85vw !important;
             font-size: 1rem !important;
         }
 
         #description {
+            position: relative !important;
             justify-self: center !important;
             width: 85vw !important;
             font-size: 0.9rem !important;
