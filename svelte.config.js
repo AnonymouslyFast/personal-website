6498c0a65f02ef5efa487ea5675a111eadd3dfcb
AnonymouslyFast <102996231+AnonymouslyFast@users.noreply.github.com>
2025-09-05 23:04:33 -0400

    Added 404 page

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: undefined
-		})
-	},
-
-	path: {
-		base: "./personal-website"
+		adapter: adapter(),
+		paths: {
+			base: '/personal-website'
+		}
 	}
 };
 
