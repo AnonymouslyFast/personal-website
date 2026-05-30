3c6e7a23a6d71f50c4bdf218a85486be0bff8959
Caleb Shearer <calebshearer1@gmail.com>
2026-05-30 19:46:38 -0400

    Website Refractor Part 4 - Starting Education Page

---
+++ src/routes/education/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Navbar from "$lib/Navbar.svelte";
+    import Copyright from "$lib/Copyright.svelte";
+</script>
+
+
+<Navbar />
+
+<div style="grid-area: content; width: 100%; height: 80vh; align-items: center; display: flex; justify-content: center;}">
+    Coming Soon (Maybe)
+</div>
+
+<Copyright/>
